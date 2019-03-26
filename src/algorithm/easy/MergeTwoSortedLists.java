b32426f0781c316022613b55749327fc9d2841a2
duyang <duyangs1994@gmail.com>
2019-03-26 10:30:04 +0800

    Add part of the question description

--- src/algorithm/easy/MergeTwoSortedLists.java
+++ src/algorithm/easy/MergeTwoSortedLists.java
@@ -5,7 +5,14 @@ package algorithm.easy;
  * @LeetCode https://leetcode-cn.com/problems/merge-two-sorted-lists/
  * @github https://github.com/duyangs
  * @date 2019/3/19
- * @description LeetCode
+ * @description 合并两个有序链表
+ *
+ * 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ *
+ * 示例：
+ *
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
  */
 public class MergeTwoSortedLists {
 
